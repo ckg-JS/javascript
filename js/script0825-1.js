6f5fe9e0ebedbfe668461d922e0f644c4ea614b6
YoshihiroUEDA <ckg17999@st.ckg.ac.jp>
2019-08-25 16:02:28 +0900

    javascript source moved.

---
+++ js/script0825-1.js
@@ -0,0 +1,11 @@
+function btnAction001_1(){
+    let item = document.getElementById( "txtNum");
+    let text = item.value;
+//            alert( text);
+    if( text == "1"){
+        alert( "happy");
+    }else{
+        alert( "unhappy");
+    }
+}
+
