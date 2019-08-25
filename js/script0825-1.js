32e835339d00a63bee69e94fe79680ee00bfe7cc
YoshihiroUEDA <ckg17999@st.ckg.ac.jp>
2019-08-25 17:45:48 +0900

    comment add.

--- js/script0825-1.js
+++ js/script0825-1.js
@@ -1,11 +1,42 @@
 function btnAction001_1(){
+    //  テキストボックスから数字をとりだす
     let item = document.getElementById( "txtNum");
     let text = item.value;
-//            alert( text);
+    alert( text);
+
+    //  キャンバスからペンを作る
+    let canvas = document.getElementById( "cCanvas");
+    alert( "getElementById() is ok");
+    let pen = canvas.getContext( "2d");
+    alert( "getContent() is ok.");
+
+    //  ペンの色を設定する
+    pen.fillStyle = "black";
+    alert( pen.fillStyle);
+
+    //  テキストボックスから取り出した文字をみて、二つに分ける
     if( text == "1"){
-        alert( "happy");
+        drawLineDownRight( pen);
     }else{
-        alert( "unhappy");
+        drawLineDownLeft( pen);
+    }
+/*    
+*/
+}
+
+function drawLineDownRight(  pen ){
+    let MAX_W = 600;
+    for ( let x = 0; x <= MAX_W ; x++){
+        pen.fillRect( x, x, 1, 1    );
     }
 }
 
+function drawLineDownLeft( pen){
+    let MAX_W = 600;
+    let MAX_H = 400;
+    for ( let x = 0; x <= MAX_W ; x++){
+        pen.fillRect( x, MAX_H - x, 1, 1    );
+
+    }
+
+}
